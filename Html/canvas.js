bde205234c7ff1ecd53f0f2c68ec77e754f00094
hema-sasi <hemasasidhar51@gmail.com>
2022-11-30 19:33:39 +0530

    ok

---
+++ Html/canvas.js
@@ -0,0 +1,14 @@
+function logo( ){
+    var canvas=document.getElementById("my canvas");
+    var ctxt=canvas.getcontext("2d");
+    ctxt.font="50pt algerian";
+    ctxt.fillstyle="red";
+    ctxt.filltext("SASIDHAR" ,30,200);
+}
+
+
+canvas(
+    background-color: white;
+)
+
+window.addEventListener('load',logo,false);
