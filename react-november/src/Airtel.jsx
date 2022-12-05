4a96546c61b6510b321b41a7562f8b89470bbbab
hema-sasi <hemasasidhar51@gmail.com>
2022-12-05 17:36:04 +0530

    ok

---
+++ react-november/src/Airtel.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Navbar/Navbar'
+let Airtel = ( ) =>{
+
+    return <div>
+        <Navbar/>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+    </div>
+}
+
+export default Airtel
